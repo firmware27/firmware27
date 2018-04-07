f1d50d2f6cd849a9bd60c6fb264212a7da6b57b1
firmware27 <37496343+firmware27@users.noreply.github.com>
2018-04-08 01:55:19 +0700

    Add files via upload

---
+++ https-foxyform.js
@@ -0,0 +1,12 @@
+	(function(d){
+	  var l = d.getElementById("foxyform_embed_link_923186");
+	      i = d.createElement('IFRAME');
+	  i.src = "https://www.foxyform.com/form.php?id=923186&sec_hash=dee21e1344d";
+	  i.width = "350px";
+	  i.height = "380px";
+	  i.frameborder = 0;
+	  i.setAttribute('style', 'border: none;');
+	  i.setAttribute('frameborder', '0');
+	  l.setAttribute('style', 'display: block; width: 350px; font-size: 0.7em; color: grey; text-align: right; text-decoration: none;');
+	  l.parentNode.insertBefore(i,l);
+	})(document);
