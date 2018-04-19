0a5a9456c60137c0c8c161c64628c5d5bb464051
firmware27 <37496343+firmware27@users.noreply.github.com>
2018-04-19 15:33:44 +0700

    Update multi-link.js

--- multi-link.js
+++ multi-link.js
@@ -18,7 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*10+ 1);var xxx=null;
+{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*2+ 1);var xxx=null;
 if(x=="1"){xxx="/2018/01/the-dazling-samsung-galaxy-s6-edge.html"}
 if(x=="2"){xxx="/2018/01/samsung-galaxy-s7-price-and-full.html"}
 if(x=="3"){xxx="/2018/01/the-best-samsung-smartphone-for-gaming.html"}
