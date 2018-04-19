4da663405f275c55bbbeb7148618a687e53b1bf1
firmware27 <37496343+firmware27@users.noreply.github.com>
2018-04-19 12:53:29 +0700

    Update multi-link.js

--- multi-link.js
+++ multi-link.js
@@ -28,7 +28,7 @@ if(x=="6"){xxx="/2018/01/iphone-7-price-and-specification-in.html"}
 if(x=="7"){xxx="/2018/01/best-smartphone-for-photography.html"}
 if(x=="8"){xxx="/2018/01/best-smartphone-for-vlogging.html"}
 if(x=="9"){xxx="/2018/01/best-smartphone-for-music.html"}
-if(x=="10"){xxx="/2018/01/best-smartphone-for-video.html"}a_to_ve[i].href="http://cloud.fw27.xyz"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="10"){xxx="/2018/01/best-smartphone-for-video.html"}a_to_ve[i].href="https://cloud.fw27.xyz"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
